723e57afc1e0489b0ecabfc6ce6de3e36d5ec62c
Lyudmil Latinov <llatinov@gmail.com>
2015-01-29 16:10:44 +0200

    Initialise project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SeleniumSamples.NET
+
+.NET Selenium WebDriver samples that are used in my blog: <a href="http://automationrhapsody.com/">Automation Rhapsody</a>
---
+++ Utf8icons/Program.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Utf8icons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Launch browser
+            IWebDriver webDriver = new FirefoxDriver();
+            webDriver.Url = "http://automationrhapsody.com/examples/utf8icons.html";
+            webDriver.Navigate();
+
+            // Locate element
+            IWebElement element = webDriver.FindElement(By.CssSelector("span.cancel"));
+            
+            // Debug info
+            bool isDisplayed = element.Displayed; // false
+            int width = element.Size.Width; // 0
+            int heigth = element.Size.Height; // 17
+
+            // Try to click -> An unhandled exception of type 'OpenQA.Selenium.ElementNotVisibleException' occurred in WebDriver.dll
+            //element.Click();
+
+            // jQuery workaround of the click
+            ((IJavaScriptExecutor)webDriver).
+                ExecuteScript("$('span.cancel').click()");
+
+            // JavaScript workaround of the click
+            ((IJavaScriptExecutor)webDriver).
+                ExecuteScript("document.getElementsByClassName('cancel')[0].click();");
+
+            // Locate element with jQuery
+            element = (IWebElement)((IJavaScriptExecutor)webDriver).ExecuteScript("return $(span.cancel')[0]");
+            // Debug info
+            isDisplayed = element.Displayed; // false
+            width = element.Size.Width; // 0
+            heigth = element.Size.Height; // 17
+        }
+    }
+}
